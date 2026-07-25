df9b466a99dea5b44fb7ec9ecb032dde2818ec30
tarun225601-star <tarun225601@gmail.com>
2026-07-25 13:29:18 +0530

    Autonomous Agent Self-Healing Commit for lib/theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-  );
+  static const Color primaryColor = Colors.blue;
+  static const Color secondaryColor = Colors.grey;
+  static const Color backgroundColor = Colors.white;
+  static const Color textColor = Colors.black;
 
-  static const darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primaryColor: Colors.blue,
-  );
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: backgroundColor,
+      textTheme: const TextTheme(
+        bodyText1: TextStyle(color: textColor),
+        bodyText2: TextStyle(color: textColor),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      scaffoldBackgroundColor: Colors.grey[900],
+      textTheme: const TextTheme(
+        bodyText1: TextStyle(color: Colors.white),
+        bodyText2: TextStyle(color: Colors.white),
+      ),
+    );
+  }
 }
