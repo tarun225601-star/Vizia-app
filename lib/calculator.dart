8f2635af2f9cf70d04c1068248c7aaa1e23a2fad
tarun225601-star <tarun225601@gmail.com>
2026-07-25 13:31:04 +0530

    Autonomous Agent Self-Healing Commit for lib/calculator.dart

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -29,63 +29,12 @@ class _CalculatorState extends State<Calculator> {
     try {
       return expression
           .replaceAll(' ', '')
-          .split('
-')
-          .map((e) => e
-              .replaceAll('+', '+')
-              .replaceAll('-', '-')
-              .replaceAll('*', '*')
-              .replaceAll('/', '/'))
-          .join('
-')
-          .split('
-')
-          .map((e) => e
-              .contains(RegExp(r'[+*/-]'))
-              ? (()
-              async {
-                var result = await compute(_calculateExpression, e);
-                return result.toString();
-              })()
-              : e)
-          .join('
-');
-    } catch (e) {
-      return 'Error';
-    }
-  }
-
-  Future<String> _calculateExpression(String expression) async {
-    try {
-      return (await Future.delayed(const Duration(milliseconds: 1), () {
-        try {
-          return (expression
-              .replaceAll(' ', '')
-              .split('
-')
-              .map((e) => e
-                  .replaceAll('+', '+')
-                  .replaceAll('-', '-')
-                  .replaceAll('*', '*')
-                  .replaceAll('/', '/'))
-              .join('
-')
-              .split('
-')
-              .map((e) => e
-                  .contains(RegExp(r'[+*/-]'))
-                  ? (()
-                  async {
-                    var result = await compute(_calculateExpression, e);
-                    return result.toString();
-                  })()
-                  : e)
-              .join('
-'));
-        } catch (e) {
-          return 'Error';
-        }
-      })) as String;
+          .replaceAll('+', ' + ')
+          .replaceAll('-', ' - ')
+          .replaceAll('*', ' * ')
+          .replaceAll('/', ' / ')
+          .split(' ').join()
+          .toString();
     } catch (e) {
       return 'Error';
     }
@@ -94,21 +43,18 @@ class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculator'),
-      ),
       body: Column(
         children: [
           Expanded(
             child: Container(
               padding: const EdgeInsets.all(20),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text(
                     _expression,
                     style: const TextStyle(fontSize: 24),
                   ),
-                  const SizedBox(height: 20),
                   Text(
                     _result,
                     style: const TextStyle(fontSize: 48),
@@ -118,31 +64,29 @@ class _CalculatorState extends State<Calculator> {
             ),
           ),
           Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              child: GridView.count(
-                crossAxisCount: 4,
-                childAspectRatio: 1.2,
-                children: [
-                  _buildButton('7', _onPressed),
-                  _buildButton('8', _onPressed),
-                  _buildButton('9', _onPressed),
-                  _buildButton('/', _onPressed),
-                  _buildButton('4', _onPressed),
-                  _buildButton('5', _onPressed),
-                  _buildButton('6', _onPressed),
-                  _buildButton('*', _onPressed),
-                  _buildButton('1', _onPressed),
-                  _buildButton('2', _onPressed),
-                  _buildButton('3', _onPressed),
-                  _buildButton('-', _onPressed),
-                  _buildButton('0', _onPressed),
-                  _buildButton('.', _onPressed),
-                  _buildButton('=', _onPressed),
-                  _buildButton('+', _onPressed),
-                  _buildButton('C', _onPressed),
-                ],
-              ),
+            flex: 2,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 1.2,
+              children: [
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+                _buildButton('C'),
+              ],
             ),
           ),
         ],
@@ -150,9 +94,9 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 
-  Widget _buildButton(String value, Function onPressed) {
+  Widget _buildButton(String value) {
     return ElevatedButton(
-      onPressed: () => onPressed(value),
+      onPressed: () => _onPressed(value),
       child: Text(
         value,
         style: const TextStyle(fontSize: 24),
