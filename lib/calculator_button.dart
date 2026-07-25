29245014804d7f5569c864a993dd4328e55c4879
tarun225601-star <tarun225601@gmail.com>
2026-07-25 12:52:24 +0530

    Autonomous Agent Self-Healing Commit for lib/calculator_button.dart

--- lib/calculator_button.dart
+++ lib/calculator_button.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-  final String label;
-  final Function onTap;
+  final Function onPressed;
+  final String buttonValue;
 
-  const CalculatorButton({
-    Key? key,
-    required this.label,
-    required this.onTap,
-  }) : super(key: key);
+  const CalculatorButton({Key? key, required this.onPressed, required this.buttonValue}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onTap();
-      },
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Center(
-          child: Text(
-            label,
-            style: const TextStyle(fontSize: 24),
-          ),
-        ),
+    return ElevatedButton(
+      onPressed: () => onPressed(buttonValue),
+      child: Text(
+        buttonValue,
+        style: const TextStyle(fontSize: 24),
       ),
     );
   }
