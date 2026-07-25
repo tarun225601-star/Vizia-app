0e0d44aa8f2898376bead53809c10c8f84880d6c
tarun225601-star <tarun225601@gmail.com>
2026-07-25 13:28:13 +0530

    Autonomous Agent Self-Healing Commit for lib/theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+  );
+
+  static const darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+  );
+}
