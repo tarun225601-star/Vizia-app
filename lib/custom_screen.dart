2d56ed4df757ef47e84b7d9bbc4a0a0b89ca76d3
tarun225601-star <tarun225601@gmail.com>
2026-07-25 13:28:10 +0530

    Autonomous Agent Self-Healing Commit for lib/custom_screen.dart

--- lib/custom_screen.dart
+++ lib/custom_screen.dart
@@ -1,5 +1,5 @@
-import 'package:calculator_app/calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:calculator_app/calculator.dart';
 
 void main() {
   runApp(const MyApp());
