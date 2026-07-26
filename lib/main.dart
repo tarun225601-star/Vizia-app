3a9f371122b669be99b9b20c9f666f6e4fd6eb91
tarun225601-star <tarun225601@gmail.com>
2026-07-26 08:59:34 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,155 @@
 import 'package:flutter/material.dart';
-import 'package:calculator_app/calculator.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Calculator App',
+      title: 'Calculator',
       home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String _currentInput = '';
+  String _result = '';
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        _currentInput = '';
+        _result = '';
+      } else if (value == '=') {
+        try {
+          _result = _calculate(_currentInput).toString();
+        } catch (e) {
+          _result = 'Error';
+        }
+      } else {
+        _currentInput += value;
+      }
+    });
+  }
+
+  double _calculate(String input) {
+    try {
+      return double.parse(input);
+    } catch (e) {
+      try {
+        return eval(input);
+      } catch (e) {
+        rethrow;
+      }
+    }
+  }
+
+  double eval(String input) {
+    try {
+      return double.parse(input);
+    } catch (e) {
+      final regex = RegExp(r'([0-9]+\.?[0-9]*)\s*([+\-*/])\s*([0-9]+\.?[0-9]*)');
+      final match = regex.firstMatch(input);
+      if (match != null) {
+        final num1 = double.parse(match.group(1)!);
+        final operator = match.group(2)!;
+        final num2 = double.parse(match.group(3)!);
+        switch (operator) {
+          case '+':
+            return num1 + num2;
+          case '-':
+            return num1 - num2;
+          case '*':
+            return num1 * num2;
+          case '/':
+            if (num2 != 0) {
+              return num1 / num2;
+            } else {
+              throw Exception('Division by zero');
+            }
+          default:
+            throw Exception('Invalid operator');
+        }
+      } else {
+        throw Exception('Invalid input');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _result.isEmpty ? _currentInput : _result,
+                style: const TextStyle(fontSize: 40),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 1.2,
+              children: [
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+                _buildButton('C'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String value) {
+    return GestureDetector(
+      onTap: () => _onButtonPressed(value),
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Center(
+          child: Text(
+            value,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
     );
   }
 }
