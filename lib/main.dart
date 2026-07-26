6953d533aa7b8ad07360a2bc66fbf25130ebac4c
tarun225601-star <tarun225601@gmail.com>
2026-07-26 09:17:56 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -46,9 +46,12 @@ class _CalculatorState extends State<Calculator> {
 
   double _calculate(String input) {
     try {
-      return Function.apply(double.parse, [input]);
+      return Function.apply(
+        {'+': (a, b) => a + b, '-': (a, b) => a - b, '*': (a, b) => a * b, '/': (a, b) => a / b}[input.contains('+') ? '+' : input.contains('-') ? '-' : input.contains('*') ? '*' : '/'],
+        [double.parse(input.split('+').first), double.parse(input.split('+').last)],
+      );
     } catch (e) {
-      throw Exception('Invalid input');
+      rethrow;
     }
   }
 
@@ -59,11 +62,11 @@ class _CalculatorState extends State<Calculator> {
         children: [
           Expanded(
             child: Container(
-              padding: const EdgeInsets.all(20),
               alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(16.0),
               child: Text(
                 _result.isEmpty ? _currentInput : _result,
-                style: const TextStyle(fontSize: 40),
+                style: const TextStyle(fontSize: 48),
               ),
             ),
           ),
@@ -73,46 +76,65 @@ class _CalculatorState extends State<Calculator> {
               crossAxisCount: 4,
               childAspectRatio: 1.2,
               children: [
-                _buildButton('7'),
-                _buildButton('8'),
-                _buildButton('9'),
-                _buildButton('/'),
-                _buildButton('4'),
-                _buildButton('5'),
-                _buildButton('6'),
-                _buildButton('*'),
-                _buildButton('1'),
-                _buildButton('2'),
-                _buildButton('3'),
-                _buildButton('-'),
-                _buildButton('0'),
-                _buildButton('.'),
-                _buildButton('='),
-                _buildButton('+'),
-                _buildButton('C'),
+                ...['7', '8', '9', '/'].map((e) => InkWell(
+                  onTap: () => _onButtonPressed(e),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text(e, style: const TextStyle(fontSize: 24))),
+                  ),
+                )).toList(),
+                ...['4', '5', '6', '*'].map((e) => InkWell(
+                  onTap: () => _onButtonPressed(e),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text(e, style: const TextStyle(fontSize: 24))),
+                  ),
+                )).toList(),
+                ...['1', '2', '3', '-'].map((e) => InkWell(
+                  onTap: () => _onButtonPressed(e),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text(e, style: const TextStyle(fontSize: 24))),
+                  ),
+                )).toList(),
+                ...['0', '.', '=', '+'].map((e) => InkWell(
+                  onTap: () => _onButtonPressed(e),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text(e, style: const TextStyle(fontSize: 24))),
+                  ),
+                )).toList(),
+                InkWell(
+                  onTap: () => _onButtonPressed('C'),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Center(child: Text('C', style: const TextStyle(fontSize: 24))),
+                  ),
+                ),
               ],
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildButton(String value) {
-    return GestureDetector(
-      onTap: () => _onButtonPressed(value),
-      child: Container(
-        margin: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Center(
-          child: Text(
-            value,
-            style: const TextStyle(fontSize: 24),
-          ),
-        ),
       ),
     );
   }
