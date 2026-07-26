c4ca33e175285e58c624da05634a5a4d0dfa9c08
tarun225601-star <tarun225601@gmail.com>
2026-07-26 12:02:43 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,166 +24,93 @@ class CalculatorPage extends StatefulWidget {
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
-  final _controller = TextEditingController(text: '0');
-  double _result = 0;
-  String _operation = '';
-  double _num1 = 0;
-  double _num2 = 0;
+  String _expression = '';
+  String _result = '';
 
-  void _calculate() {
+  void _onPressed(String value) {
     setState(() {
-      _num1 = double.parse(_controller.text);
-      switch (_operation) {
-        case '+':
-          _result = _num1 + _num2;
-          break;
-        case '-':
-          _result = _num1 - _num2;
-          break;
-        case '*':
-          _result = _num1 * _num2;
-          break;
-        case '/':
-          _result = _num1 / _num2;
-          break;
-        default:
-          _result = 0;
-      }
-      _controller.text = _result.toString();
-    });
-  }
-
-  void _clear() {
-    setState(() {
-      _controller.text = '0';
-      _result = 0;
-      _operation = '';
-      _num1 = 0;
-      _num2 = 0;
-    });
-  }
-
-  void _setOperation(String operation) {
-    setState(() {
-      _num1 = double.parse(_controller.text);
-      _operation = operation;
-      _controller.text = '0';
-    });
-  }
-
-  void _setNumber(String number) {
-    setState(() {
-      if (_controller.text == '0') {
-        _controller.text = number;
+      if (value == '=') {
+        _result = _calculate(_expression);
+        _expression = '';
+      } else if (value == 'C') {
+        _expression = '';
+        _result = '';
       } else {
-        _controller.text += number;
+        _expression += value;
       }
     });
+  }
+
+  String _calculate(String expression) {
+    try {
+      return expression.eval().toString();
+    } catch (e) {
+      return 'Error';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calculator App'),
+        title: const Text('Calculator'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              readOnly: true,
-              textAlign: TextAlign.right,
-              style: const TextStyle(fontSize: 24),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    _expression,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    _result,
+                    style: const TextStyle(fontSize: 48),
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          ),
+          Expanded(
+            flex: 2,
+            child: GridView.count(
+              crossAxisCount: 4,
+              childAspectRatio: 1.2,
               children: [
-                ElevatedButton(
-                  onPressed: () => _setNumber('7'),
-                  child: const Text('7'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('8'),
-                  child: const Text('8'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('9'),
-                  child: const Text('9'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setOperation('/'),
-                  child: const Text('/'),
-                ),
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('C'),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _setNumber('4'),
-                  child: const Text('4'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('5'),
-                  child: const Text('5'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('6'),
-                  child: const Text('6'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setOperation('*'),
-                  child: const Text('*'),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _setNumber('1'),
-                  child: const Text('1'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('2'),
-                  child: const Text('2'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setNumber('3'),
-                  child: const Text('3'),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setOperation('-'),
-                  child: const Text('-'),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _setNumber('0'),
-                  child: const Text('0'),
-                ),
-                ElevatedButton(
-                  onPressed: _clear,
-                  child: const Text('C'),
-                ),
-                ElevatedButton(
-                  onPressed: _calculate,
-                  child: const Text('='),
-                ),
-                ElevatedButton(
-                  onPressed: () => _setOperation('+'),
-                  child: const Text('+'),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(String value) {
+    return ElevatedButton(
+      onPressed: () => _onPressed(value),
+      child: Text(
+        value,
+        style: const TextStyle(fontSize: 24),
       ),
     );
   }
