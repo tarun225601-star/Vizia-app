9402257c3260b1f965aebccef08595560416a840
tarun225601-star <tarun225601@gmail.com>
2026-07-26 09:22:46 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,62 @@
 import 'package:flutter/material.dart';
 
-class CalculatorApp extends StatelessWidget {
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CalculatorHomePage(),
+    return const MaterialApp(
+      title: 'Calculator',
+      home: Calculator(),
     );
   }
 }
 
-class CalculatorHomePage extends StatefulWidget {
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
   @override
-  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _CalculatorHomePageState extends State<CalculatorHomePage> {
+class _CalculatorState extends State<Calculator> {
   String _currentInput = '';
   String _result = '';
-  double _num1 = 0;
-  double _num2 = 0;
-  String _operation = '';
 
-  void _onButtonPressed(String buttonText) {
+  void _onButtonPressed(String value) {
     setState(() {
-      if (buttonText == 'C') {
+      if (value == 'C') {
         _currentInput = '';
         _result = '';
-        _num1 = 0;
-        _num2 = 0;
-        _operation = '';
-      } else if (buttonText == '+' || buttonText == '-' || buttonText == '*' || buttonText == '/') {
-        _num1 = double.parse(_currentInput);
-        _operation = buttonText;
-        _currentInput = '';
-      } else if (buttonText == '=') {
-        _num2 = double.parse(_currentInput);
-        if (_operation == '+') {
-          _result = (_num1 + _num2).toString();
-        } else if (_operation == '-') {
-          _result = (_num1 - _num2).toString();
-        } else if (_operation == '*') {
-          _result = (_num1 * _num2).toString();
-        } else if (_operation == '/') {
-          _result = (_num1 / _num2).toString();
+      } else if (value == '=') {
+        try {
+          _result = _calculate(_currentInput);
+        } catch (e) {
+          _result = 'Error';
         }
-        _currentInput = '';
       } else {
-        _currentInput += buttonText;
+        _currentInput += value;
       }
     });
+  }
+
+  String _calculate(String input) {
+    try {
+      return input
+          .replaceAll(' ', '')
+          .replaceAll('+', ' + ')
+          .replaceAll('-', ' - ')
+          .replaceAll('*', ' * ')
+          .replaceAll('/', ' / ')
+          .split(' ').join()
+          .toString();
+    } catch (e) {
+      return 'Error';
+    }
   }
 
   @override
@@ -60,35 +64,39 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              _currentInput + _result,
-              style: TextStyle(fontSize: 24),
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                _result.isEmpty ? _currentInput : _result,
+                style: const TextStyle(fontSize: 40),
+              ),
             ),
           ),
           Expanded(
+            flex: 3,
             child: GridView.count(
               crossAxisCount: 4,
               childAspectRatio: 1.2,
               children: [
-                CalculatorButton('7', _onButtonPressed),
-                CalculatorButton('8', _onButtonPressed),
-                CalculatorButton('9', _onButtonPressed),
-                CalculatorButton('/', _onButtonPressed),
-                CalculatorButton('4', _onButtonPressed),
-                CalculatorButton('5', _onButtonPressed),
-                CalculatorButton('6', _onButtonPressed),
-                CalculatorButton('*', _onButtonPressed),
-                CalculatorButton('1', _onButtonPressed),
-                CalculatorButton('2', _onButtonPressed),
-                CalculatorButton('3', _onButtonPressed),
-                CalculatorButton('-', _onButtonPressed),
-                CalculatorButton('0', _onButtonPressed),
-                CalculatorButton('.', _onButtonPressed),
-                CalculatorButton('=', _onButtonPressed),
-                CalculatorButton('+', _onButtonPressed),
-                CalculatorButton('C', _onButtonPressed),
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+                _buildButton('C'),
               ],
             ),
           ),
@@ -96,31 +104,23 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
       ),
     );
   }
-}
 
-class CalculatorButton extends StatelessWidget {
-  final String _buttonText;
-  final Function _onButtonPressed;
-
-  CalculatorButton(this._buttonText, this._onButtonPressed);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildButton(String value) {
     return GestureDetector(
-      onTap: () => _onButtonPressed(_buttonText),
+      onTap: () => _onButtonPressed(value),
       child: Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(5),
         decoration: BoxDecoration(
           color: Colors.grey[200],
           borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
           child: Text(
-            _buttonText,
-            style: TextStyle(fontSize: 24),
+            value,
+            style: const TextStyle(fontSize: 24),
           ),
         ),
       ),
     );
   }
-}
+  }
