7b2063a6bd5281d730d005c0d177bb70f90504cc
tarun225601-star <tarun225601@gmail.com>
2026-07-26 07:21:36 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,56 +24,40 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  final _controller = TextEditingController();
-  String _result = '';
+  final _controller = TextEditingController(text: '0');
+  double _result = 0;
+  String _operator = '';
+  double _num1 = 0;
+  double _num2 = 0;
 
-  void _calculate(String operator) {
+  void _calculate() {
     setState(() {
-      if (operator == '=') {
-        _result = _calculateResult(_controller.text);
-      } else {
-        _controller.text += operator;
-      }
+      _num1 = double.parse(_controller.text);
     });
   }
 
-  String _calculateResult(String expression) {
-    try {
-      return expression
-          .replaceAll(' ', '')
-          .split('
-')
-          .map((e) => e
-              .replaceAll('+', ' + ')
-              .replaceAll('-', ' - ')
-              .replaceAll('*', ' * ')
-              .replaceAll('/', ' / ')
-              .split(' ')
-              .map((e) => double.parse(e))
-              .reduce((value, element) {
-                switch (expression
-                    .replaceAll(' ', '')
-                    .split('
-')[0]
-                    .split(RegExp(r'\d+'))
-                    .last) {
-                  case '+':
-                    return value + element;
-                  case '-':
-                    return value - element;
-                  case '*':
-                    return value * element;
-                  case '/':
-                    return value / element;
-                  default:
-                    return 0;
-                }
-              }))
-          .join('
-');
-    } catch (e) {
-      return 'Error';
-    }
+  void _operatorButton(String operator) {
+    setState(() {
+      _num1 = double.parse(_controller.text);
+      _operator = operator;
+      _controller.clear();
+    });
+  }
+
+  void _equalButton() {
+    setState(() {
+      _num2 = double.parse(_controller.text);
+      if (_operator == '+') {
+        _result = _num1 + _num2;
+      } else if (_operator == '-') {
+        _result = _num1 - _num2;
+      } else if (_operator == '*') {
+        _result = _num1 * _num2;
+      } else if (_operator == '/') {
+        _result = _num1 / _num2;
+      }
+      _controller.text = _result.toString();
+    });
   }
 
   @override
@@ -83,23 +67,100 @@ class _CalculatorState extends State<Calculator> {
         title: const Text('Simple Calculator'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             TextField(
               controller: _controller,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter expression',
-              ),
+              readOnly: true,
+              textAlign: TextAlign.right,
+              style: const TextStyle(fontSize: 24),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _calculate('='),
-              child: const Text('Calculate'),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.text += '7',
+                  child: const Text('7'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '8',
+                  child: const Text('8'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '9',
+                  child: const Text('9'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _operatorButton('/'),
+                  child: const Text('/'),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            Text(_result),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.text += '4',
+                  child: const Text('4'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '5',
+                  child: const Text('5'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '6',
+                  child: const Text('6'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _operatorButton('*'),
+                  child: const Text('*'),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.text += '1',
+                  child: const Text('1'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '2',
+                  child: const Text('2'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '3',
+                  child: const Text('3'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _operatorButton('-'),
+                  child: const Text('-'),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.text += '0',
+                  child: const Text('0'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _controller.text += '.',
+                  child: const Text('.'),
+                ),
+                ElevatedButton(
+                  onPressed: _equalButton,
+                  child: const Text('='),
+                ),
+                ElevatedButton(
+                  onPressed: () => _operatorButton('+'),
+                  child: const Text('+'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
