b700100b55c6faf85233ae76c1edfe0853f486e2
tarun225601-star <tarun225601@gmail.com>
2026-07-26 10:16:36 +0530

    Autonomous Agent Self-Healing Commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,77 @@
 import 'package:flutter/material.dart';
 
-class CalculatorApp extends StatelessWidget {
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calculator App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CalculatorHomePage(),
+    return const MaterialApp(
+      title: 'Calculator',
+      home: Calculator(),
     );
   }
 }
 
-class CalculatorHomePage extends StatefulWidget {
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
   @override
-  _CalculatorHomePageState createState() => _CalculatorHomePageState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _CalculatorHomePageState extends State<CalculatorHomePage> {
+class _CalculatorState extends State<Calculator> {
   String _currentInput = '';
   String _result = '';
-  double _num1 = 0;
-  double _num2 = 0;
-  String _operation = '';
 
   void _onButtonPressed(String value) {
     setState(() {
       if (value == 'C') {
         _currentInput = '';
         _result = '';
-        _num1 = 0;
-        _num2 = 0;
-        _operation = '';
-      } else if (value == '+' || value == '-' || value == '*' || value == '/') {
-        _num1 = double.parse(_currentInput);
-        _operation = value;
-        _currentInput = '';
       } else if (value == '=') {
-        _num2 = double.parse(_currentInput);
-        if (_operation == '+') {
-          _result = (_num1 + _num2).toString();
-        } else if (_operation == '-') {
-          _result = (_num1 - _num2).toString();
-        } else if (_operation == '*') {
-          _result = (_num1 * _num2).toString();
-        } else if (_operation == '/') {
-          _result = (_num1 / _num2).toString();
+        try {
+          _result = _calculate(_currentInput).toString();
+        } catch (e) {
+          _result = 'Error';
         }
-        _currentInput = '';
       } else {
         _currentInput += value;
       }
     });
+  }
+
+  double _calculate(String input) {
+    try {
+      return Function.apply(
+        _getFunction(input),
+        _getArguments(input),
+      );
+    } catch (e) {
+      throw Exception('Invalid input');
+    }
+  }
+
+  Function _getFunction(String input) {
+    if (input.contains('+')) {
+      return (a, b) => a + b;
+    } else if (input.contains('-')) {
+      return (a, b) => a - b;
+    } else if (input.contains('*')) {
+      return (a, b) => a * b;
+    } else if (input.contains('/')) {
+      return (a, b) => a / b;
+    } else {
+      throw Exception('Invalid input');
+    }
+  }
+
+  List<double> _getArguments(String input) {
+    final parts = input.split(RegExp(r'[+\-*/]'));
+    return parts.map((e) => double.parse(e)).toList();
   }
 
   @override
@@ -60,35 +79,49 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Text(
-              _currentInput + _result,
-              style: TextStyle(fontSize: 24),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _currentInput.isEmpty ? '0' : _currentInput,
+                style: const TextStyle(fontSize: 40),
+              ),
             ),
           ),
           Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                _result.isEmpty ? '' : _result,
+                style: const TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
             child: GridView.count(
               crossAxisCount: 4,
               childAspectRatio: 1.2,
               children: [
-                CalculatorButton('7', _onButtonPressed),
-                CalculatorButton('8', _onButtonPressed),
-                CalculatorButton('9', _onButtonPressed),
-                CalculatorButton('/', _onButtonPressed),
-                CalculatorButton('4', _onButtonPressed),
-                CalculatorButton('5', _onButtonPressed),
-                CalculatorButton('6', _onButtonPressed),
-                CalculatorButton('*', _onButtonPressed),
-                CalculatorButton('1', _onButtonPressed),
-                CalculatorButton('2', _onButtonPressed),
-                CalculatorButton('3', _onButtonPressed),
-                CalculatorButton('-', _onButtonPressed),
-                CalculatorButton('0', _onButtonPressed),
-                CalculatorButton('.', _onButtonPressed),
-                CalculatorButton('=', _onButtonPressed),
-                CalculatorButton('+', _onButtonPressed),
-                CalculatorButton('C', _onButtonPressed),
+                _buildButton('7'),
+                _buildButton('8'),
+                _buildButton('9'),
+                _buildButton('/'),
+                _buildButton('4'),
+                _buildButton('5'),
+                _buildButton('6'),
+                _buildButton('*'),
+                _buildButton('1'),
+                _buildButton('2'),
+                _buildButton('3'),
+                _buildButton('-'),
+                _buildButton('0'),
+                _buildButton('.'),
+                _buildButton('='),
+                _buildButton('+'),
+                _buildButton('C'),
               ],
             ),
           ),
@@ -96,28 +129,20 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
       ),
     );
   }
-}
 
-class CalculatorButton extends StatelessWidget {
-  final String _value;
-  final Function _onButtonPressed;
-
-  CalculatorButton(this._value, this._onButtonPressed);
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildButton(String value) {
     return GestureDetector(
-      onTap: () => _onButtonPressed(_value),
+      onTap: () => _onButtonPressed(value),
       child: Container(
-        margin: EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
+          color: Colors.blue,
           borderRadius: BorderRadius.circular(10),
         ),
         child: Center(
           child: Text(
-            _value,
-            style: TextStyle(fontSize: 24),
+            value,
+            style: const TextStyle(fontSize: 24, color: Colors.white),
           ),
         ),
       ),
